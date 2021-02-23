4fd311817d5e92defcba19501e59597ab0da8691
Sarah Marguerettaz <sarah.marg47@gmail.com>
2021-02-23 12:36:10 +0100

    banière v1

---
+++ front/includes/commons/Baniere_presentation.php
@@ -0,0 +1,29 @@
+
+ <!DOCTYPE html>
+    <link rel="stylesheet" href="C:\wamp64\www\Bubble_Groupe7_Blogart\front\includes\commons\Baniere_presentation.css">
+  
+<div class= "first page">   
+    <div class="baniere">
+            <div class="logo">
+                <img src="" alt="Logo Les faces cachés">
+            </div>
+            
+            <div class="titreH1">
+                <h1>Les faces cachés de bordeaux</h1>
+            </div>
+
+            <div class="image banière">
+                <img src="" alt="les quais Bordealais">
+            </div>
+    </div>
+    <div class="présentation">
+        <div class ="présentation_titre">
+            <h2>Présentation</h2>
+        </div>
+
+        <div class="presentation_texte">
+            <p>Laissez-vous guider par notre blog “Les Faces Cachées de Bordeaux”. Ce blog saura vous montrer le chemin vers toutes les surprises que nous réserve notre ville ! Des visages qui se cachent et vous observent en silence en passant par des événements historiques, plus rien n’aura de secret pour vous ! Prêt.e à tenter l’aventure ? 
+            </p>
+        </div>
+    </div> 
+</div>
