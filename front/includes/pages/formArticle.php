6a03d5fdd55ca8be11c1de603b095970b3f5669d
Alyssalade <galydraw@gmail.com>
2021-02-24 14:32:35 +0100

    Connexion + Gestions de tous les crud + début création article

---
+++ front/includes/pages/formArticle.php
@@ -0,0 +1,43 @@
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../../assets/css/formArticle.css" />
+    </head>
+
+    <div class="container">
+        <h1> écrivez votre article </h1>
+        <form method="post" action="traitement.php">
+            <label for="titre">Titre de votre article</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="100 caractères maximum" title="Renseignez le titre de votre article" /> </br>
+
+            <label for="chapeau">Chapeau de l'article</label> </br>
+            <input type="text" name="prenom" id="prenom" placeholder="500 caractères maximum" title="Renseignez le chapeau de votre article" /> </br>
+
+            <label for="accroche">Phrase d'accroche</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="100 caractères maximum" title="Renseignez la phrase d'accroche de votre article" /> </br>
+
+            <label for="sous-titre1">Sous-titre du premier paragraphe</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="100 caractères maximum" title="Renseignez le sous-titre de votre premier paragraphe" /> </br>
+
+            <label for="paragraphe1">Premier paragraphe</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="1200 caractères maximum" title="Renseignez le premier paragraphe de votre article" /> </br>
+
+            <label for="sous-titre2">Sous-titre du deuxième paragraphe</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="100 caractères maximum" title="Renseignez le sous-titre de votre deuxième paragraphe" /> </br>
+
+            <label for="paragraphe2">Deuxième paragraphe</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="1200 caractères maximum" title="Renseignez le deuxième paragraphe de votre article" /> </br>
+
+            <label for="paragraphe3">Troisième paragraphe</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="1200 caractères maximum" title="Renseignez le troisième paragraphe de votre article" /> </br>
+
+            <label for="conclusion">Conclusion</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="800 caractères maximum" title="Renseignez la conclusion de votre article" /> </br>
+
+            <label for="tag">Tags</label> </br>
+            <input type="text" name="nom" id="nom" placeholder="60 caractères maximum par mots clés" title="Renseignez les tags de votre article" /> </br>
+        </form>
+    </div>
+
+</html>
