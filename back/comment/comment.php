1a981f2d6d77b70131a21026db44fc3d3959a41f
mthibault006 <thibault.maxime@outlook.com>
2021-02-26 10:34:34 +0100

    corraction liens + CSS tableau Angle

--- back/comment/comment.php
+++ back/comment/comment.php
@@ -25,67 +25,73 @@ $comment = new COMMENT;
 
     <link rel="stylesheet" href="../../front/assets/css/normalize.css">
     <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/gestionCRUD.css" >
 
 </head>
-<body>
-    <h1>BLOGART21 Admin - Gestion du CRUD Commentaire</h1>
+<body class="fullpage">
+<div class="hautpage">
+    <div class="Titre">
+        <h1>BLOGART21 Admin - Gestion du CRUD Commentaire</h1>
 
-    <h2>Tous les commentaires</h2>
-    <hr /><br />
-    <h2>Nouveau commentaire:&nbsp;<a href="./createComment.php"><i>Créer un commentaire</i></a></h2>
-    <br /><hr />
-    <h2>Tous les commentaires</h2>
+        <h2>Tous les commentaires</h2>
 
-    <br><br>
+    </div>
 
-    <table border="3" bgcolor="aliceblue">
-    <thead>
-        <tr>
-            <th>&nbsp;NumSeqCom&nbsp;</th>
-            <th>&nbsp;NumArticle&nbsp;</th>
-            <th>&nbsp;DtCreCom&nbsp;</th>
-            <th>&nbsp;LibCom&nbsp;</th>
-            <th>&nbsp;AttModOK&nbsp;</th>
-            <th>&nbsp;AffComOK&nbsp;</th>
-            <th>&nbsp;NotifComKOAff&nbsp;</th>
-            <th>&nbsp;delLogiq&nbsp;</th>
-            <th>&nbsp;numMemb&nbsp;</th>
-            <th colspan="2">&nbsp;Action&nbsp;</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?
-    $allComment = $comment->get_AllComment();
-    foreach($allComment as $row){
-    // Appel méthode : tous les commentaires en BDD
+    <div class="creerBt">
+        <button class="button" onclick="location.href='./createComment.php'">
+            Créer un commentaire
+        </button>
+    </div>
+</div>
+    <table class="tableau">
+        <thead class="entete">
+            <tr>
+                <th>&nbsp;NumSeqCom&nbsp;</th>
+                <th>&nbsp;NumArticle&nbsp;</th>
+                <th>&nbsp;DtCreCom&nbsp;</th>
+                <th>&nbsp;LibCom&nbsp;</th>
+                <th>&nbsp;AttModOK&nbsp;</th>
+                <th>&nbsp;AffComOK&nbsp;</th>
+                <th>&nbsp;NotifComKOAff&nbsp;</th>
+                <th>&nbsp;delLogiq&nbsp;</th>
+                <th>&nbsp;numMemb&nbsp;</th>
+                <th colspan="2">&nbsp;Action&nbsp;</th>
+            </tr>
+        </thead>
+        <tbody class="body">
+            <?
+            $allComment = $comment->get_AllComment();
+            foreach($allComment as $row){
+            // Appel méthode : tous les commentaires en BDD
 
-    // Boucle pour afficher
-    //foreach($all as $row) {
-    ?>
-        <tr>
-        <td><h4>&nbsp; <?php echo $row["numSeqCom"]; ?> &nbsp;</h4></td>
-        <td>&nbsp; <?php echo $row["numArt"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["dtCreCom"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["libCom"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["attModOK"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["affComOK"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["notifComKOAff"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["delLogiq"]; ?> &nbsp;</td>
-        <td>&nbsp; <?php echo $row["numMemb"]; ?> &nbsp;</td>
-        <?
-            $numSeqCom = $row["numSeqCom"];
-            $numArt = $row["numArt"];
-        ?>
-        <td>&nbsp;<a href="./updateComment.php?id=<?=$numSeqCom."&id2=".$numArt;?>"><i>Valider</i></a>&nbsp;
-        <br /></td>
-        <td>&nbsp;<a href="./deleteComment.php?id=<?=$numSeqCom."&id2=".$numArt;?>"><i>Supprimer</i></a>&nbsp;
-        <br /></td>
-        </tr>
-    <?
-    }	// End of foreach
-    ?>
-    </tbody>
+            // Boucle pour afficher
+            //foreach($all as $row) {
+            ?>
+                <tr>
+                <td><h4>&nbsp; <?php echo $row["numSeqCom"]; ?> &nbsp;</h4></td>
+                <td>&nbsp; <?php echo $row["numArt"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["dtCreCom"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["libCom"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["attModOK"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["affComOK"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["notifComKOAff"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["delLogiq"]; ?> &nbsp;</td>
+                <td>&nbsp; <?php echo $row["numMemb"]; ?> &nbsp;</td>
+                <?
+                    $numSeqCom = $row["numSeqCom"];
+                    $numArt = $row["numArt"];
+                ?>
+                <td>&nbsp;<a class="button" href="./updateComment.php?id=<?=$numSeqCom."&id2=".$numArt;?>"><i>Valider</i></a>&nbsp;
+                <br /></td>
+                <td>&nbsp;<a class="button" href="./deleteComment.php?id=<?=$numSeqCom."&id2=".$numArt;?>"><i>Supprimer</i></a>&nbsp;
+                <br /></td>
+                </tr>
+            <?
+            }	// End of foreach
+            ?>
+            </tbody>
     </table>
+</div>
 
     <br><br>
 
