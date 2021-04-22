b97d86e81962c6ca086838f18b4e158a638abf07
mthibault006 <thibault.maxime@outlook.com>
2021-04-22 21:31:04 +0200

    correction liens connect bdd

--- CONNECT/config.php
+++ CONNECT/config.php
@@ -1,14 +1,14 @@
 <?
 
   // nom de votre serveur (ou 127.0.0.1)
-  $hostBD = "plateforme-mmi.iut.u-bordeaux-montaigne.fr";
+  $hostBD = "localhost";
   // nom BD
-  $nomBD = "wp_user07";
+  $nomBD = "blogart21";
   // Serveur
   // Avec encodage UTF8
   $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
 
   // nom utilisateur de connexion à la BDD
-  $userBD = 'user_db_07';         // Votre login
+  $userBD = 'root';         // Votre login
   // mot de passe de connexion à la BDD
-  $passBD = 'mmi-2020';         // Votre Pass
+  $passBD = '';         // Votre Pass
